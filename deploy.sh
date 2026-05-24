fd775ebd0862381993175f70d350bfd2ac896bcd
Alessandro Perales <alessandropc07@gmail.com>
2026-05-24 01:27:26 +0000

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,13 @@
 
 PROJECT_ID="mlops-deploy-497300"
 
+IMAGE_URI="us-central1-docker.pkg.dev/$PROJECT_ID/ataque-cardiaco-api/ataque-cardiaco-api"
+
 gcloud builds submit \
---tag gcr.io/$PROJECT_ID/ataque-cardiaco-api
+--tag $IMAGE_URI
 
 gcloud run deploy ataque-cardiaco-api \
---image gcr.io/$PROJECT_ID/ataque-cardiaco-api \
+--image $IMAGE_URI \
 --platform managed \
 --region us-central1 \
 --allow-unauthenticated
